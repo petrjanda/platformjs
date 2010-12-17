4e807402c4ce8129c52b9ce0bc4553048395f29c
Petr Janda <petrjanda@me.com>
2010-12-17 19:38:38 +0100

    client reconnect support

--- public/chat.js
+++ public/chat.js
@@ -2,6 +2,8 @@ var Chat = function() {
 	
 	var self = this;
 	
+	self.buffer = [];
+	
 	self.ws = null;
 }
 
@@ -19,51 +21,75 @@ Chat.prototype.write = function(content) {
 						"</div>");
 }
 
+Chat.prototype.connect = function() {
+	
+	var self = this;
+	
+	var server = $('#lblServer').val();
+	
+	self.write("Connecting server ws://" + server);
+	
+	if ("WebSocket" in window) {
+	  	self.ws = new WebSocket("ws://" + server);
+
+		self.ws.onopen = function() {
+			self.write("Socket opened");
+			
+			while(self.buffer.length)
+				self.send(self.buffer.shift());
+		};
+
+	  	self.ws.onmessage = function (evt) { 
+			self.write(event.data);
+		};
+
+	  	self.ws.onclose = function() {
+			self.write("Socket closed");
+		};
+	} 
+	else {
+		self.write("Browser doesn't support WebSockets!");
+	}
+}
+
 Chat.prototype.init = function() {
 	
 	var self = this;
 	
 	$("#btnSubmit").click(function() {
-		self.ws.send($('#lblInput').val());
-		$('#lblInput').val('');
-		$('#lblInput').focus();				
+		self.send();			
 	});
 	
 	$('#lblInput').keydown(function(event) {
-		if(event.keyCode == 13) {
-			self.ws.send($('#lblInput').val());
-			$('#lblInput').val('');
-			$('#lblInput').focus();
-		}	
+		if(event.keyCode == 13)
+			self.send();
 	});
 	
 	$('#input').hide();
 
 	$('#btnConnect').click(function() {
 		
-		self.write("Connecting server ws://" + $('#lblServer').val());
-		
-		if ("WebSocket" in window) {
-		  	self.ws = new WebSocket("ws://" + $('#lblServer').val());
-
-			self.ws.onopen = function() {
-				self.write("Socket opened");
-			};
-
-		  	self.ws.onmessage = function (evt) { 
-				self.write(event.data);
-			};
-
-		  	self.ws.onclose = function() {
-				self.write("Socket closed");
-			};
-		} 
-		else {
-			self.write("Browser doesn't support WebSockets!");
-		}	
+		self.connect();
 		
 		$('#connect').hide();
 		$('#input').show();
 		$('#lblInput').focus();	
 	});
 };
+
+Chat.prototype.send = function() {
+	
+	var self = this,
+		message = $('#lblInput').val();
+
+	if(self.ws.readyState == WebSocket.CLOSED)
+	{
+		self.buffer.push(message)
+		self.connect();
+		return;
+	}
+		
+	self.ws.send(message);
+	$('#lblInput').val('');
+	$('#lblInput').focus();
+}
