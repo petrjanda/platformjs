fd73dc4f9924e32c86158f24210275834d67ea02
Petr Janda <petrjanda@me.com>
2010-12-19 15:25:08 +0100

    channels support

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,7 @@
 require.paths.unshift(__dirname + "/lib");
 
+var sys = require('sys');
+
 var	PlatformJS = require('server'),
 	connect = require('connect/index');
 
@@ -9,5 +11,8 @@ var server = connect.createServer(
 
 var platformjs = new PlatformJS(server);
 
+var port = process.argv[2] == "-p" ? process.argv[3] : 8000;
 // start webserver listening for new requests
-server.listen(8000);
+server.listen(port);
+
+sys.puts('=> HTTP server started on port ' + port);
