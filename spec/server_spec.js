a994de2a3eff871c8b9fbfe4b19bbe18b0972e12
Petr Janda <petrjanda@me.com>
2011-01-11 19:37:58 +0100

    improved server spec, modified clients class

--- spec/server_spec.js
+++ spec/server_spec.js
@@ -1,17 +1,62 @@
 require.paths.unshift(__dirname + "../lib");
 
 var	PlatformJS = require('server'),
-	connect = require('connect/index');
+	connect = require('connect/index'),
+	WebSocket = require('websocket').WebSocket,
+	sys = require('sys');
 
-describe("Server", function() {
+describe("server", function() {
 	
-	var server = connect.createServer(
-		connect.staticProvider({root: './public'})
-	);
+	var platformjs = new PlatformJS(server);
+	var server = connect.createServer();
 	
-	it("should be able to hook on the server", function() {
-		expect(server._events.upgrade).toBeUndefined();
-		var platformjs = new PlatformJS(server);
-		expect(server._events.upgrade).toBeDefined();
+	server.listen(8000);
+	
+	beforeEach(function() {
+		platformjs.listen(server);
+	});
+	
+	afterEach(function() {
+		platformjs.close();
+	});
+	
+	describe("components", function() {
+		
+		it("should have client manager", function() {
+			expect(platformjs.clients).toBeDefined();
+		});
+		
+		it("should have channels manager", function() {
+			expect(platformjs.channels).toBeDefined();
+		});
+		
+		it("should have storage manager", function() {
+			expect(platformjs.storage).toBeDefined();
+		})
+		
+	})
+	
+	describe("client", function() {
+
+		it("should be able to connect", function() {
+			var client = new WebSocket('ws://localhost:8000/');
+			var size = 0;
+			
+			waitsFor(function() {
+			    var key;
+				for (key in platformjs.clients.clients)
+				    if(platformjs.clients.clients.hasOwnProperty(key)) size++;
+				return size > 0;
+			}, "Client didn't connect!", 1000);
+			
+			expect(true).toBeTruthy();
+		});
+	});
+	
+	describe("hooks", function () {
+
+		it("should be prepared after server start", function() {
+			expect(server._events.upgrade).toBeDefined();
+		});
 	});
 });
--- specs.sh
+++ specs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/env node specs.js $@
+/usr/bin/env node --debug specs.js $@
