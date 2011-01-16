e3d09c49b2503642ee9a214e0b91549fc0606e9d
Petr Janda <petrjanda@me.com>
2011-01-16 19:05:30 +0100

    added tests folder, added jsdom and should to submodules, modified gitignore, added init script for tests

---
+++ test/init.js
@@ -0,0 +1,4 @@
+require.paths.unshift(__dirname + "./../deps/tobi/lib");
+require.paths.unshift(__dirname + "./../deps/jsdom/lib");
+require.paths.unshift(__dirname + "./../deps/should/lib");
+require.paths.unshift(__dirname + "./../lib");
